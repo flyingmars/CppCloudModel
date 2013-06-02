7860584c3c5d2cfdbd535db059ff8ffd5483a780
Mars-Win7 <flyingmars@gmail.com>
2013-06-03 00:08:03 +0800

    change main

--- Main.cpp
+++ Main.cpp
@@ -9,7 +9,7 @@ int main ( int argc , char ** argv ){
 	
 	
 	Initializer::baseState_OneDimension_Initialization(grid);
-	Initializer::perturbation_Initialization(grid);
+	Initializer::perturbation_Initialization_Cold(grid);
 
 	Outputter::output_BaseState(grid);
 	LeapFrogMethod::compute_all(grid,FINISHTIME);
